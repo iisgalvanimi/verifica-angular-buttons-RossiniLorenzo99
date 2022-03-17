ec92e0623827206f83132a33acb1867cd3f79cbc
RossiniLorenzo99 <rossini.lorenzo@iisgalvanimi.edu.it>
2022-03-17 08:58:58 +0000

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Empty-Project';
+  
+  list: string[] = []
+  testo1:string="button1";
+  testo2:string="button2";
+  testo3:string="button3";
+  testo4:string="button4";
+
+  ultimo:string= ""
+
+  constructor() {}
+
+  onClick1() {
+    this.list.push(this.testo1)
+    this.ultimo = this.testo1
+  }
+
+  onClick2() {
+    this.list.push(this.testo2) 
+    this.ultimo = this.testo2
+  }
+
+  onClick3() {
+    this.list.push(this.testo3)
+    this.ultimo = this.testo3
+  }
+
+  onClick4() {
+    this.list.push(this.testo4)
+    this.ultimo = this.testo4
+  }
+
+
+
 }
